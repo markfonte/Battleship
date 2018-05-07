8da46d162674b14bbec0e4f93fc8faf8b94e083f
root <root@MFONTE-201706.clg.local>
2018-05-07 16:04:43 -0400

    init

---
+++ app/src/main/java/com/example/mfonte/battleship/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.mfonte.battleship;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startApp();
+    }
+    protected void startApp() {
+        Intent i = new Intent(this, Lobby.class);
+        startActivity(i);
+    }
+    
+    protected void sendGetRequest()  {
+        final TextView mTextView = findViewById(R.id.textBox1);
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://10.0.2.2/";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("errorInfo", response);
+                        mTextView.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("errorInfo", error.getMessage());
+                mTextView.setText("Error using GET request");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
